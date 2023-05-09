d56c61f06f070280acdb0408c0dbd701f1394369
Dilip V <dilipvishwakarma7878@gmail.com>
2023-05-09 18:39:25 +0530

    update

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
+AOS.init({
+    duration: 600, // values from 0 to 3000, with step 50ms
+    mirror: true, // whether elements should animate out while scrolling past them
+});
+
+
 const brandsSwiper = new Swiper('.brands-swiper', {
     loop: true,
     autoplay: true,
     slidesPerView: 6,
     spaceBetween: 50,
+    grabCursor: true
 });
 
 
@@ -10,11 +17,16 @@ const testiSwiper1 = new Swiper('.testi-swiper-img', {
     loop: true,
     autoplay: true,
     slidesPerView: 1,
+    grabCursor: true,
+    pagination: {
+        el: '.swiper-pagination',
+    },
 });
 
 const testiSwiper2 = new Swiper('.testi-swiper-txt', {
     loop: true,
     slidesPerView: 1,
+    grabCursor: true
 });
 
 testiSwiper1.controller.control = testiSwiper2;
