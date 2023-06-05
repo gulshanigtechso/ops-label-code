7089cea08ac3786ec2b395a8b85632e18c6baaf7
gulshan_songara <gulshansongara@igtechso.com>
2023-06-05 17:36:29 +0530

    Added PHPMailer for Contact-form

--- mail.php
+++ mail.php
@@ -1,16 +1,37 @@
 <?php
-// get data from form
-$name = $_POST['name'];
-$email = $_POST['email'];
-$message = $_POST['message'];
-$subj = $_POST['subj'];
-$to = "contact@opslabel.com";
-$subject = "New Form Submission";
-$txt = "Name= " . $name . "\r\nEmail= " . $email . "\r\nSubject= " . $subj . "\r\nMessage= " . $message;
+use PHPMailer\PHPMailer\PHPMailer;
 
-if ($email != NULL) {
-    mail($to, $subject, $txt);
+require_once 'phpmailer/Exception.php';
+require_once 'phpmailer/PHPMailer.php';
+require_once 'phpmailer/SMTP.php';
+
+$mail = new PHPMailer(true);
+
+$alert = '';
+
+if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $subj = $_POST['subj'];
+
+    try {
+        $mail->isSMTP();
+        $mail->Host = 'smtp-relay.sendinblue.com';
+        $mail->SMTPAuth = true;
+        $mail->Username = 'contact@opslabel.com';
+        $mail->Password = '9ys5k4Br7zUDZOHV';
+        $mail->SMTPSecure = 'tls';
+        $mail->Port = '587';
+        $mail->setFrom($email);
+        $mail->addAddress('contact@opslabel.com');
+        $mail->isHTML(true);
+        $mail->Subject = 'New Form Submission';
+        $mail->Body = "Name: $name <br>Email: $email <br>Subject: $subj <br>Message: $message";
+        $mail->send();
+        header("Location:contactSuccess.html");
+    } catch (Exception $e) {
+        $alert = "<div><h1>NOT Sent</h1></div>";
+    }
 }
-// redirect
-header("Location:contactSuccess.html");
 ?>
